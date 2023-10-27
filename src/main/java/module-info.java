29fdcddd9da14641b22f70da38a2b7fea988e8c5
PiMichal <michal.pieroog@gmail.com>
2023-10-27 14:16:38 +0200

    Initial view added to controller, no logic implemented

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module WeatherApp {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires com.google.gson;
+
+    opens pl.pierogmichal;
+    opens pl.pierogmichal.controller;
+    opens pl.pierogmichal.view;
+}
--- src/main/java/pl/pierogmichal/Main.java
+++ src/main/java/pl/pierogmichal/Main.java
@@ -1,6 +1,15 @@
 package pl.pierogmichal;
 
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import pl.pierogmichal.view.ViewFactory;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory();
+        viewFactory.showMainViewWindow();
+    }
 }
